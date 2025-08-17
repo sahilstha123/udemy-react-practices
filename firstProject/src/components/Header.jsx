783010a53f698924a14149f81043ff8671a1d3cb
sahilstha123 <sthasahil401@gmail.com>
2025-08-17 23:06:51 +0545

    complete header components

---
+++ firstProject/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import reactLogo from "../assets/reactLogo.png";
+const Header = () => {
+  return (
+    <div className="container mx-auto">
+      <header>
+        <div className="flex justify-center">
+          <img src={reactLogo} alt="" width="250px" height="150px" />
+        </div>
+        <div className="flex flex-col items-center flex-wrap md:flex-nowrap text-center px-3 ">
+          <h1 className=" mb-3 font-bold text-6xl bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500  bg-clip-text text-transparent animate-bounce">React Essentials</h1>
+          <p className="text-purple-500 text-2xl md:text-3xl font-light">
+            Fundamental React Concepts you will need for almost any app you are
+            going to build
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
