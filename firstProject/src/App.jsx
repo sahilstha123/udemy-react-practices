c0c87b9772b69cfd6014d7266101e254f3a95e73
sahilstha123 <sthasahil401@gmail.com>
2025-08-22 00:06:58 +0545

    modify app.jsx file

--- firstProject/src/App.jsx
+++ firstProject/src/App.jsx
@@ -9,25 +9,23 @@ function App() {
     <div className="bg-indigo-950 min-h-screen">
       <div className="container mx-auto">
         <Header />
-        <section className=" flex justify-center">
-          <div className="bg-black p-6 rounded-lg  w-5xl m-4 ">
-            <main>
+        <main>
+          {/* coreconcept */}
+          <section className=" flex justify-center">
+            <div className="bg-black p-6 rounded-lg  w-5xl m-4 ">
               <h2 className=" text-2xl font-semibold text-purple-500 mb-4 text-center">
                 Core Concepts
               </h2>
               <div className="flex gap-3 flex-wrap flex-grow">
                 {/* <Coreconcept {...CORE_CONCEPTS[0]} /> */}
-                {
-                  CORE_CONCEPTS.map((item,index)=>{
-                    return <Coreconcept key = {index} {...item}/>
-                  })
-                }
-               
+                {CORE_CONCEPTS.map((item, index) => {
+                  return <Coreconcept key={index} {...item} />;
+                })}
               </div>
-            </main>
-          </div>
-        </section>
-        <Footer/>
+            </div>
+          </section>
+        </main>
+        <Footer />
       </div>
     </div>
   );
