3a945663d1dcaf234d44dc237d1636453fead4e4
sahilstha123 <sthasahil401@gmail.com>
2025-08-27 11:47:59 +0545

    modify app file

--- firstProject/src/App.jsx
+++ firstProject/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import Example from "./components/Example";
+import Concepts from "./components/concepts";
 
 function App() {
   const [selectTopic, setSelectTopic] = useState("");
@@ -22,17 +23,7 @@ function App() {
         <main>
           {/* coreconcept */}
           <section className=" flex justify-center">
-            <div className="bg-black p-6 rounded-lg  w-5xl m-4 ">
-              <h2 className=" text-2xl font-semibold text-purple-500 mb-4 text-center">
-                Core Concepts
-              </h2>
-              <div className="flex gap-3 flex-wrap flex-grow">
-                {/* <Coreconcept {...CORE_CONCEPTS[0]} /> */}
-                {CORE_CONCEPTS.map((item, index) => {
-                  return <Coreconcept key={index} {...item} />;
-                })}
-              </div>
-            </div>
+            <Concepts/>
           </section>
 
           {/* example */}
