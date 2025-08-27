dd65013831533c6cd50e932fe288d474e3266d08
sahilstha123 <sthasahil401@gmail.com>
2025-08-27 11:49:09 +0545

    modify tabbutton

--- firstProject/src/components/Tabbutton.jsx
+++ firstProject/src/components/Tabbutton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Tabbutton = ({children, onCLick,isSelected}) => {
+const Tabbutton = ({children, isSelected ,...props}) => {
   return (
     <div>
-        <li className={`list-none  ${isSelected? "bg-purple-600 hover:bg-purple-700 ":"bg-none"} hover:bg-gray-900 inline px-3 py-2 rounded-md text-gray-300`}><button onClick={onCLick} className='cursor-pointer'>{children}</button></li>
+        <li className={`list-none  ${isSelected? "bg-purple-600 hover:bg-purple-700 ":"bg-none"} hover:bg-gray-900 inline px-3 py-2 rounded-md text-gray-300`}><button {...props} className='cursor-pointer'>{children}</button></li>
     </div>
   )
 }
