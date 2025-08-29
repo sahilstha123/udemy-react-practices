9aa90c3411273a423fe5a8caea09619a3f1daae0
sahilstha123 <sthasahil401@gmail.com>
2025-08-29 13:57:49 +0545

    update players

--- Tic-Tac-Toe/src/components/Player.jsx
+++ Tic-Tac-Toe/src/components/Player.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
 
 const Player = ({ player, symbol }) => {
+  const [playerName, setPlayerName] = useState(player);
   const [isEditing, setisEditing] = useState(false);
 
   const handleOnClick = () => {
-    setisEditing(true);
+    setisEditing((editing) => !editing);
   };
 
+  const handleOnChange = (e) => {
+    setPlayerName(e.target.value);
+  };
+
+  const keyDown = (e)=>{
+    if(e.key === "Enter")
+    {
+        setisEditing(false)
+    }
+  }
   return (
     <li className="flex flex-col sm:flex-row gap-2 items-start sm:items-center">
       <span className="flex gap-4 flex-wrap items-center">
         {isEditing ? (
           <input
             type="text"
-            placeholder="player name"
+            // defaultValue={player}
             className="bg-gray-400 px-3 py-1 w-full sm:w-32 max-w-[150px] focus:outline-none rounded-md mr-4"
+            value={playerName}
+            onChange={handleOnChange}
+            onKeyDown={keyDown}
           />
         ) : (
-          <span>{player}</span>
+          <span>{playerName}</span>
         )}
 
         <span className="flex items-center">{symbol}</span>
@@ -25,9 +39,9 @@ const Player = ({ player, symbol }) => {
 
       <button
         onClick={handleOnClick}
-        className="bg-blue-500 hover:bg-blue-600 px-3 py-1 rounded-md text-sm w-[90%] sm:w-24"
+        className="bg-blue-500 hover:bg-blue-600 px-3 py-1 rounded-md text-sm w-[80%]  sm:w-24"
       >
-        Edit
+        {isEditing ? "Save" : "Edit"}
       </button>
     </li>
   );
