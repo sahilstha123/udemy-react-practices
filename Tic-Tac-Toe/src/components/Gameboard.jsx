bebca16d5fad4b34dc4eb0550879dcbf2f8fee60
sahilstha123 <sthasahil401@gmail.com>
2025-08-29 19:37:36 +0545

    complete the button click

--- Tic-Tac-Toe/src/components/Gameboard.jsx
+++ Tic-Tac-Toe/src/components/Gameboard.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const initialGameboard = [
-  ["sahil", "sahil", "sahil"],
-  ["sahil", "sahil", "sahil"],
-  ["sahil", "sahil", "sahil"],
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
 const Gameboard = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameboard);
+
+  const handleOnClick = (rowIndex, colIndex) => {
+    setGameBoard((prevGameboard) => {
+      const updatedGameboard = prevGameboard.map(innerArry => [...innerArry]);
+      updatedGameboard[rowIndex][colIndex] = "X";
+      console.log(updatedGameboard);
+      return updatedGameboard;
+    });
+  };
+
   return (
     <ol className="flex flex-col items-center gap-4 m-9">
-      {initialGameboard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol className="flex gap-4">
             {row.map((playerSymbol, colIndex) => (
@@ -19,10 +30,12 @@ const Gameboard = () => {
                     primary-color text-white font-semibold
                     w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28
                     flex items-center justify-center
-                     rounded
+                    rounded
                     transition-transform hover:scale-105
                     cursor-pointer
+                    text-4xl font-serif
                   "
+                  onClick={() => handleOnClick(rowIndex, colIndex)}
                 >
                   {playerSymbol}
                 </button>
