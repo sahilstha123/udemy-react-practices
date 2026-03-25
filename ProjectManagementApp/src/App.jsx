fba3ed5c2c86d140f501fe669ebc1b078383266a
Sahil shrestha <sthasahil401@gmail.com>
2026-03-25 20:30:42 +0545

    wip: working on addNewProject

--- ProjectManagementApp/src/App.jsx
+++ ProjectManagementApp/src/App.jsx
@@ -3,15 +3,57 @@ import { useState } from 'react'
 import './App.css'
 import ProjectSidebar from './components/ProjectSidebar'
 import NewProject from './components/NewProject'
+import NoProjectSelected from './components/NoProjectSelected'
+import { toast, ToastContainer } from 'react-toastify'
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  const handleStartAddProject = () => {
+    setProjectsState(prev => (
+      {
+        ...prev,
+        selectedProjectId: null
+      }
+    ))
+  }
+
+  const handleAddProject = (projectData) => {
+
+   
+    setProjectsState(prevState => {
+      const isDuplicate = prevState.projects.some(items => (
+        items.title.toLowerCase().trim() === projectData.title.toLowerCase().trim()
+      ))
+      if (isDuplicate) {
+        toast("Title already exist")
+        return prevState
+      }
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+  console.log(projectsState)
 
   return (
-
-    <main className='h-screen my-8 flex gap-8'>
-      <ProjectSidebar />
-      <NewProject />
-    </main>
+    <>
+      <main className='h-screen my-8 flex gap-8'>
+        <ProjectSidebar handleStartAddProject={handleStartAddProject} />
+        {projectsState.selectedProjectId === null ? (<NewProject AddNewProject={handleAddProject} />) : (
+          <NoProjectSelected handleStartAddProject={handleStartAddProject} />
+        )}
+      </main>
+      <ToastContainer />
+    </>
 
   )
 }
--- ProjectManagementApp/src/components/NewProject.jsx
+++ ProjectManagementApp/src/components/NewProject.jsx
@@ -1,30 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from './Input'
 
-const NewProject = () => {
+const NewProject = ({ AddNewProject }) => {
+
+    const titleRef = useRef()
+    const descriptionRef = useRef()
+    const dueDateRef = useRef()
     const fields = [
         {
             label: "Title",
-
+            required: true,
+            ref: titleRef,
         },
         {
             label: "Description",
-            textarea: true
+            textarea: true,
+            required: true,
+            ref: descriptionRef,
+
         },
         {
-            label: "Due Date"
+            label: "Due Date",
+            ref: dueDateRef,
+            required: true,
+            type: "date",
         }
     ]
+
+    const handleSave = () => {
+        
+        const projectData = {
+            title: titleRef.current.value,
+            description: descriptionRef.current.value,
+            dueDate: descriptionRef.current.value
+        }
+        AddNewProject(projectData)
+
+        titleRef.current.value = "",
+        descriptionRef.current.value = "",
+        dueDateRef.current.value = ""
+    }
     return (
         <div className='w-140 mt-16'>
             <menu className='flex items-center justify-end gap-4 my-4'>
                 <li><button className='text-stone-800 hover:text-stone-950 cursor-pointer'>Cancel</button></li>
-                <li><button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950 cursor-pointer'> Save</button></li>
+                <li><button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950 cursor-pointer' onClick={handleSave}> Save</button></li>
             </menu>
 
             <div>
                 {fields.map((items, index) => (
-                    <Input key={index} {...items} />
+                    <Input key={index} {...items} ref={items.ref} />
                 ))}
             </div>
         </div>
